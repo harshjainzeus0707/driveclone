23434b49978ec216aab8cb34f5792025b64ddf75
sagar dhandhalya <dhandhalya.sagar@searce.com>
2021-09-24 15:26:12 +0530

    seach bar done color

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,9 +6,9 @@ import SearchIcon from "@mui/icons-material/Search";
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
   borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  backgroundColor: alpha(theme.palette.common.white, 0.5),
   "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+    backgroundColor: alpha(theme.palette.common.white, 0.7),
   },
   marginLeft: 0,
   width: "100%",
